1d3d388fa5f1d8d8b8ced4bef28124f5a03dc8c0
fedorath <kurtax.h1@googlemail.com>
2017-04-20 14:40:36 +0100

    Update motion_detector.py

--- motion_detector.py
+++ motion_detector.py
@@ -1,46 +1,41 @@
-# import the necessary packages
-import argparse
-import datetime
-import imutils
-import time
-import cv2
- 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
-args = vars(ap.parse_args())
- 
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
-	camera = cv2.VideoCapture(0)
-	time.sleep(0.25)
- 
-# otherwise, we are reading from a video file
-else:
-	camera = cv2.VideoCapture(args["video"])
- 
-# initialize the first frame in the video stream
-firstFrame = None
+# FACE DETECTION
+# --------------------
+# Command line version
+# --------------------
+# Simple face-detecting script for the camera module using SimpleCV.
+# This will continuously take images and search them for himan features. Which features can 
+# be set on the line 'faces = img.findHaarFeatures('face.xml').
+# As an out put it will print the coordinates for each face detected.
+#--------------------- 
 
-# loop over the frames of the video
-while True:
-	# grab the current frame and initialize the occupied/unoccupied
-	# text
-	(grabbed, frame) = camera.read()
-	text = "Unoccupied"
- 
-	# if the frame could not be grabbed, then we have reached the end
-	# of the video
-	if not grabbed:
-		break
- 
-	# resize the frame, convert it to grayscale, and blur it
-	frame = imutils.resize(frame, width=500)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (21, 21), 0)
- 
-	# if the first frame is None, initialize it
-	if firstFrame is None:
-		firstFrame = gray
-		continue
+print 'Initialize'	# SimpleCV is BIG so it takes a good 5 seconds to import
+from SimpleCV import Display, Image
+import os			
+
+filename = 'frame'	# Save each loop's image under the same name so it's overwritten
+
+try:
+	print 'Press Ctrl+C to exit.'
+	while True:
+		# Take an image using raspistill. The small resolution helps speed things up
+		print 'Capturing...'
+		os.system('raspistill -n -w 500 -h 500 -o %s.jpg' % filename)
+		img = Image('%s.jpg' % filename)	# Pass the captured image to SimpleCV
+
+		print 'Searching...'
+		# This is the bit that does all the work:
+		# Try putting 'upper_body.xml' or 'eye.xml' in place of 'face.xml'
+		faces = img.findHaarFeatures('face.xml') 
+
+		if faces != []: # If any faces are found
+			print 'Face(s) found at:'
+			for face in faces:
+				# Print the coordinates for each face found
+				coOrd = face.coordinates()
+				print '%s' % coOrd
+		else:
+			print 'No faces found.'
+		print '--------------------'	# This just breaks things up for easy viewing
+
+except KeyboardInterrupt:
+	print 'Exit'
